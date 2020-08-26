cc41606889bbdb320e19b74e53e8a89493cc1aad
Victor Henrique <victorhenriquegamer@gmail.com>
2020-08-25 22:08:21 -0300

    Aprimorando o componente botão

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native'
 
-export default props => {
-    return (
-        <TouchableHighlight onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
-        </TouchableHighlight>
-    )
-}
-
 const styles = StyleSheet.create({
     button: {
         fontSize: 40,
@@ -20,5 +12,31 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#888',
         // borderRadius: (Dimensions.get('window').width / 4) / 2
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4 ) * 2
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4 ) * 3
     }
 })
+
+export default props => {
+
+    // Estilo padrão
+    const stylesButton = [styles.button]
+
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operation) stylesButton.push(styles.operationButton)
+
+    return (
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}>{props.label}</Text>
+        </TouchableHighlight>
+    )
+}
