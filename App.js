291e848f93264ab39bddb070a1e6c52e3fd86a26
Victor Henrique <victorhenriquegamer@gmail.com>
2020-08-25 23:31:24 -0300

    Lógica da calculadora

--- App.js
+++ App.js
@@ -1,54 +1,81 @@
-import React, { useState } from 'react'
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import React, { Component } from 'react'
+import { View, SafeAreaView, StyleSheet } from 'react-native'
 
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
-export default props => {
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
 
-  const [displayValue, setDisplayValue] = useState('0')
+export default class App extends Component {
+  state = { ...initialState }
 
-  const addDigit = n => {
-    setDisplayValue(n)
+  addDigit = n => {
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+
+    if (n === '.' && !clearDisplay
+      && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
   }
 
-  const clearMemory = () => {
-    setDisplayValue('0')
+  clearMemory = () => {
+    this.setState({ ...initialState })
   }
 
-  const setOperation = operation => {
+  setOperation = operation => {
 
   }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Display value={displayValue} />
-      <View style={styles.buttons}>
-        <Button label="AC" triple onClick={clearMemory} />
-        <Button label="/" operation onClick={setOperation} />
-        <Button label="7" onClick={addDigit}/>
-        <Button label="8" onClick={addDigit}/>
-        <Button label="9" onClick={addDigit}/>
-        <Button label="*" operation onClick={setOperation}/>
-        <Button label="4" onClick={addDigit}/>
-        <Button label="5" onClick={addDigit}/>
-        <Button label="6" onClick={addDigit}/>
-        <Button label="-" operation onClick={setOperation}/>
-        <Button label="1" onClick={addDigit}/>
-        <Button label="2" onClick={addDigit}/>
-        <Button label="3" onClick={addDigit}/>
-        <Button label="+" operation onClick={setOperation} />
-        <Button label="0" double onClick={addDigit}/>
-        <Button label="." onClick={addDigit}/>
-        <Button label="=" operation onClick={setOperation} />
-      </View>
-    </SafeAreaView>
-  )
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+          <Button label="AC" triple onClick={this.clearMemory} />
+          <Button label="/" operation onClick={this.setOperation} />
+          <Button label="7" onClick={() => this.addDigit('7')} />
+          <Button label="8" onClick={() => this.addDigit('8')} />
+          <Button label="9" onClick={() => this.addDigit('9')} />
+          <Button label="*" operation onClick={this.setOperation('*')} />
+          <Button label="4" onClick={() => this.addDigit('4')} />
+          <Button label="5" onClick={() => this.addDigit('5')} />
+          <Button label="6" onClick={() => this.addDigit('6')} />
+          <Button label="-" operation onClick={this.setOperation('-')} />
+          <Button label="1" onClick={() => this.addDigit('1')} />
+          <Button label="2" onClick={() => this.addDigit('2')} />
+          <Button label="3" onClick={() => this.addDigit('3')} />
+          <Button label="+" operation onClick={this.setOperation('+')} />
+          <Button label="0" double onClick={() => this.addDigit('0')} />
+          <Button label="." onClick={() => this.addDigit('.')} />
+          <Button label="=" operation onClick={this.setOperation} />
+        </View>
+      </SafeAreaView>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,    
+    flex: 1,
   },
   buttons: {
     flexDirection: 'row',
--- src/components/Display.js
+++ src/components/Display.js
@@ -4,10 +4,7 @@ import { View, Text, StyleSheet } from 'react-native'
 export default props => {
     return (
         <View style={styles.display}>
-            <Text 
-                style={styles.displayValue} 
-                numberOfLines={1}
-            >{props.value}</Text>
+            <Text style={styles.displayValue} numberOfLines={1}>{props.value}</Text>
         </View>
     )
 }
